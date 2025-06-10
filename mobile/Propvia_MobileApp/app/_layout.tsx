f55dbb29e3db4af0e970d53d855a52561e0abc64
ImNotStupidBro <c.blair@marquette.edu>
2025-06-10 15:08:23 -0400

    Summary Comments
    -Added comments to clarify the use of certain files.
    - PLEASE MAKE AN EFFORT TO DO THIS WITH ALL SUBSEQUENT FILES GOING FORWARD.

--- mobile/Propvia_MobileApp/app/_layout.tsx
+++ mobile/Propvia_MobileApp/app/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
-
+{/* 
+  This file is used to define the root layout of the application.
+  If you want to change the layout of the application, you should do so here.
+  THIS LAYOUT FILE SHOULD BE USED FOR GLOBAL CHANGES ONLY.
+*/}
 export default function RootLayout() {
   return <Stack />;
 }
--- mobile/Propvia_MobileApp/app/index.tsx
+++ mobile/Propvia_MobileApp/app/index.tsx
@@ -1,5 +1,8 @@
 import { Text, View } from "react-native";
-
+{/*
+    This file is the main entry point for mobile version of the Propvia website.
+    It is used to render the main page of the website and allow access to any other pages the website may have.
+*/}
 export default function Index() {
   return (
     <View
