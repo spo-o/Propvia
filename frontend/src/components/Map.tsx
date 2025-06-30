7fa0cbd857a2dec464745eeabee4f258e96f9d6f
Kassim Ballout <kassimballout@gmail.com>
2025-06-30 11:56:48 -0400

    fixed map page errors

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -4,7 +4,7 @@ import 'leaflet/dist/leaflet.css';
 import { useEffect } from 'react';
 import L from 'leaflet';
 import { getValidProperties, getBounds, getMarkerIcon } from '../utils/mapHelpers';
-import { fetchAllProperties } from '../api/map';
+// import { fetchAllProperties } from '../api/map';
 
 
 
@@ -19,7 +19,7 @@ interface MapProps {
 export default function Map({ properties, selectedProperty, onPropertySelect }: MapProps) {
   useEffect(() => {
     // Fix Leaflet icon issue
-    delete L.Icon.Default.prototype._getIconUrl;
+    delete (L.Icon.Default.prototype as any)._getIconUrl;
     L.Icon.Default.mergeOptions({
       iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
       iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
